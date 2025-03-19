09e30e774cefc9aa0c688c3b03d15fcd20a1658e
DanKhan1 <chaudriali034@gmail.com>
2025-03-19 16:08:05 +0500

    Third commit: 1st question done of assignment

---
+++ Class And Object/Question1.java
@@ -0,0 +1,19 @@
+class Complex{
+    int imaginary;
+    int real;
+    void setcomplex(int i,int r){
+        imaginary = i;
+        real = r;
+    }
+    void printcomplex(){
+        System.out.println("imaginary "+imaginary);
+        System.out.println("real "+real);
+    }
+}
+class Question1{
+    public static void main(String[] args) {
+        Complex d1 = new Complex();
+        d1.setcomplex(20, 11);
+        d1.printcomplex();
+    }
+}
